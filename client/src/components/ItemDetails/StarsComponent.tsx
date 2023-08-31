5d4ba61c0042b47bd4c5fea9b7b0591764918fdc
Shingo Morimoto <shingo@wolfssl.com>
2023-08-31 14:09:07 +0900

    BugFix: In SPTMarket.sol modify "==" to "="

--- client/src/components/ItemDetails/StarsComponent.tsx
+++ client/src/components/ItemDetails/StarsComponent.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactStars from 'react-stars';
 
-export default function StarsComponent({ratingChange}:{ratingChange: any })
+
+export default function StarsComponent({ratingChange,  stars}:{ratingChange: any, stars: number})
 {  
     
     return(
         <div>
             <ReactStars
-            value={1}
-            count={6}
+            value={stars}
+            count={5}
             size={24}
             half={false}
             onChange={ratingChange}
